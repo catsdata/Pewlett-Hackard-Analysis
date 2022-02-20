eff998f5a2b0566a329c102254fbb0feab4cd2cc
Cathy Potter <96197979+catsdata@users.noreply.github.com>
2022-02-20 09:46:56 -0500

    deliverable 1

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,34 +1,25 @@
--- Steps from 1 to 7
--- Retrieve employees' info for the ones born between 1952 and 1955
--- Retrieve title and window of employment as well
+-- 1 through 7: find employees born between 1952 and 1955
 SELECT e.emp_no, e.first_name, e.last_name,
 		t.title, t.from_date, t.to_date
 INTO retirement_titles	
 FROM employees e LEFT JOIN titles t ON (e.emp_no = t.emp_no)
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no;
--- 133776 records
 
--- Steps 8 to 15
--- Previous query filtered by active employees only
--- The order by to_date does not make sense because we are already using DISTINCT ON
+-- 8 to 15: filter for active employees only
 SELECT DISTINCT ON (r.emp_no)
 		r.emp_no, r.first_name, r.last_name, r.title
 INTO unique_titles	
 FROM retirement_titles r
 WHERE r.to_date = '9999-01-01'
 ORDER BY r.emp_no, r.to_date desc
--- 72458 records
--- 90398 without the date condition
 
-
--- Steps 16 to 21
--- counting retiring people by each title
+-- 16 to 21:  final retiring count by title
 SELECT count(*) as total, title
 INTO retiring_titles
 FROM unique_titles ut
 GROUP BY title
 ORDER BY 1 DESC
 
-
+-- print table for challenge deliverable 1
 select * from retiring_titles
